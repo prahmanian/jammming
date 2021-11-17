40ca538ea082267c20a1dd44617575bac1287657
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 18:47:32 -0800

    refactored App to class Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,22 +1,25 @@
+import React from 'react'
 import './App.css';
 
 import SearchBar from '../../Components/SearchBar/SearchBar.js'
 import SearchResults from '../../Components/SearchResults/SearchResults.js'
 import Playlist from '../../Components/Playlist/Playlist.js'
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className='App-playlist'>
-          <SearchResults />
-          <Playlist />
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className='App-playlist'>
+            <SearchResults />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
-export default App;
