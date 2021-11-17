611d914ac56ac2181414d1b45998d8fbc6a29ef4
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 18:51:52 -0800

    added state to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,7 +5,24 @@ import SearchBar from '../../Components/SearchBar/SearchBar.js'
 import SearchResults from '../../Components/SearchResults/SearchResults.js'
 import Playlist from '../../Components/Playlist/Playlist.js'
 
+const track = {
+  name: "Jammin'",
+  artist: "Pedram",
+  album: "LateNite React-ions",
+  id: 1234
+
+}
+
 export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchResults: [
+        track, track, track, track
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
