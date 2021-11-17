7e877cb50f02f2c0e3f9e903389db70c5e2dea18
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 18:12:33 -0800

    Added JSX to SearchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default class SearchBar extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className='SearchBar'>
+                <input placeholder="Enter A Song, Album, or Artist" />
+                <button className="SearchButton">SEARCH</button>
             </div>
         )
     }
