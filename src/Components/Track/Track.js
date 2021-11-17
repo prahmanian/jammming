e3fa6b57ee3010596ee66e6e12a4d658ccf68e52
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 18:44:20 -0800

    refactored Track and added styles

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -7,9 +7,17 @@ const track = {
     artist: "static artist",
     album: 'static album',
     conditional: true,
+    isRemoval: true
 }
 
 export default class Track extends React.Component {
+
+    renderAction(){
+        track.isRemoval
+            ? <button className="Track-action">-</button>
+            : <button className="Track-action">+</button>
+    }
+
     render() {
         // const {track} =this.props
         return(
@@ -18,7 +26,8 @@ export default class Track extends React.Component {
                     <h3>{track.name}</h3>
                     <p>{track.artist} | {track.album}</p>
                 </div>
-                <button className="Track-action">{track.conditional ? "+" : "-"}</button>
+                {/* <button className="Track-action">{track.conditional ? "+" : "-"}</button> */}
+                {this.renderAction()}
             </div>
         )
     }
