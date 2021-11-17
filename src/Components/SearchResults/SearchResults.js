f15ebfcf6336ff6eda4fa20cfd8eebccf648b646
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 18:18:23 -0800

    Added SearchResults JSX and style rules

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import './SearchResults.css'
+
+// import Tracklist from '../TrackList/TrackList.js'
 
 export default class SearchResults extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* <TrackList /> */}
             </div>
         )
     }
