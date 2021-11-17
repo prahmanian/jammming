be74db98c17425fe7d8ccb8312f7d04810f5dfe6
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 18:25:06 -0800

    Built out Playlist JSX and styles

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Playlist.css'
+
+// import Tracklist from '../TrackList/TrackList.js'
 
 export default class Playlist extends React.Component {
     render() {
         return (
-            <div>
+            <div className="Playlist">
+                <input defaultValue={"New Playlist"}/>
+                {/* <TrackList /> */}
+                <button class='Playlist-save'>SAVE TO SPOTIFY</button>
 
             </div>
         )
